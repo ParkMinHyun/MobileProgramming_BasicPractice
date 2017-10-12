f4ba5d6c7626c5abd636adeecda59a429112d32a
박민현 <algusl92@naver.com>
2017-10-12 23:14:07 +0900

    setResult(RESULT_OK, intent), setResult(RESULT_CANCEL) 을 이용한 Data 전달 구분

--- MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MainActivity.java
+++ MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.parkminhyun.mobileprogramming_practice;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
-
+    public static final int REQUEST_CODE_MENU = 101;// 다른액티비티를띄우기위한요청코드정의
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.editText);// 뷰객체참조
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String data = editText.getText().toString();// 입력상자에입력된전화번호확인
-                // 전화걸기화면을보여줄인텐트객체생성
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(data));
-                startActivity(intent);// 액티비티띄우기
+    }
+    public void onButton1Clicked(View v) {
+// 또다른액티비티를띄우기위한인텐트객체생성
+        Intent intent= new Intent(getApplicationContext(), MenuActivity.class);
+        intent.putExtra("hi","ABC");
+        startActivityForResult(intent, REQUEST_CODE_MENU);// 액티비티띄우기
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode== REQUEST_CODE_MENU) {
+            Toast.makeText(getApplicationContext(), "onActivityResult메소드호출됨. 요청코드: " + requestCode+ ", 결과코드: " + resultCode, Toast.LENGTH_LONG).show();
+            if (resultCode== RESULT_OK) {
+                String name = data.getExtras().getString("name");
+                Toast.makeText(getApplicationContext(), "응답으로전달된name : " + name, Toast.LENGTH_LONG).show();
             }
-        });
+        }
     }
 }
--- MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MenuActivity.java
+++ MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MenuActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +19,18 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Toast.makeText(getApplicationContext(),intent.getExtras().getString("hi"),Toast.LENGTH_LONG).show();
 
-
+        editText = (EditText) findViewById(R.id.editText2);
         Button button= (Button) findViewById(R.id.button2);// 버튼객체참조
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 Intent intent= new Intent(); // 인텐트객체생성하고name의값을부가데이터로넣기
-                intent.putExtra("name", "mike");
-                setResult(RESULT_OK, intent); // 응답보내기
+                if(editText.getText().length() == 0)
+                    setResult(RESULT_CANCELED);
+                else {
+                    intent.putExtra("name", "mike");
+                    setResult(RESULT_OK, intent); // 응답보내기
+                }
                 finish();// 현재액티비티없애기
             }
         });
