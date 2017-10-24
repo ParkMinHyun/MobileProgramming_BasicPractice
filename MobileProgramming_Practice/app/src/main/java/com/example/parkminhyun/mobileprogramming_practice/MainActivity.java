b3cd5363ffa0c521729d83e363f147308f5eb877
박민현 <algusl92@naver.com>
2017-10-24 16:12:26 +0900

    서비스 예제 적용

--- MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MainActivity.java
+++ MobileProgramming_Practice/app/src/main/java/com/example/parkminhyun/mobileprogramming_practice/MainActivity.java
@@ -1,109 +1,45 @@
 package com.example.parkminhyun.mobileprogramming_practice;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn,btn2;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText= (EditText) findViewById(R.id.editText);
 
-        btn = (Button) findViewById(R.id.infoAccessbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), Information.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                startActivity(intent);
-            }
-        });
-
-
-        btn2 = (Button) findViewById(R.id.menuAccessbtn);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void onInfoButtonClicked(View v) {
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-//        Toast.makeText(this, "onStart호출됨", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-//        Toast.makeText(this, "onStop호출됨", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        Toast.makeText(this, "onDestroy호출됨", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        Toast.makeText(this, "onPause호출됨", Toast.LENGTH_SHORT).show();
-//        saveState();// 현재입력상자에입력된데이터를저장
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        Toast.makeText(this, "onResume호출됨", Toast.LENGTH_SHORT).show();
-//        restoreState();// 설정정보에저장된데이터를복원
-    }
-
-    protected void restoreState() {
-        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
-        if ((pref != null) && (pref.contains("name"))) {
-            String name = pref.getString("name", "");
-//            nameInput.setText(name);
-        }
-    }
-
-    protected void saveState() {
-        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-//        String A = nameInput.getText().toString();
-//        A += "&";
-//        A += studentId.getText().toString();
-//        A += "&";
-//        A += studentName.getText().toString();
-
-//        editor.putString("name", A);
-        editor.commit();
-    }
-
-    protected void clearMyPrefs() {
-        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.clear();
-        editor.commit();
+        Intent passedIntent= getIntent();
+        processIntent(passedIntent);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
+        processIntent(intent);
         super.onNewIntent(intent);
     }
+
+    private void processIntent(Intent intent) {
+        if (intent != null) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+            Toast.makeText(this, "command : " + command + ", name : " + name, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onButton1Click(View v){
+        String name = editText.getText().toString();
+        Intent intent= new Intent(this, MyService.class);
+        intent.putExtra("command", "show");
+        intent.putExtra("name", name);
+        startService(intent);
+    }
+
 }
