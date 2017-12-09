f41e6e05609fc43733c9ec88e19ed4fbb966a2f9
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-12-09 23:42:30 +0900

    터치 이벤트 실습

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -1,42 +1,44 @@
 package com.example.parkminhyun.mobileprogramming;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.textView);
 
+        // 뷰를 터치했을때 이벤트를 전달 받을 리스너 등록
+        View view= findViewById(R.id.view);
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+
+                float curX= motionEvent.getX();// 터치한곳의좌표
+                float curY= motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("손가락눌림: " + curX+ ", " + curY);
+                }
+                else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락움직임: " + curX+ ", " + curY);
+                }
+                else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락뗌: " + curX+ ", " + curY);
+                }return true;}
+        });
     }
-
-    public void startBtnClick(View view) {
-        Intent intent= new Intent(this, MyService.class);
-        intent.putExtra("command", "show");
-        intent.putExtra("name", "ParkMinHyun");
-        startService(intent);
-    }
-
-    public void stopBtnClick(View view) {
-    }
-
-    // 액티비티가 이미 메모리에 만들어져있는 경우 onNewIntent() 메소드호출됨
-    @Override
-    protected void onNewIntent(Intent intent) {
-        processIntent(intent);
-        super.onNewIntent(intent);
-    }
-
-    private void processIntent(Intent intent) {
-        if (intent != null) {
-            String command = intent.getStringExtra("command");
-            String name = intent.getStringExtra("name");
-            Toast.makeText(this, "command : " + command + ", name : " + name, Toast.LENGTH_LONG).show();
-        }
+    public void println(String data) {
+        textView.append(data + "\n");
     }
 }
