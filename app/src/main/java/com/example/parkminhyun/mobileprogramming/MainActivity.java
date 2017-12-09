e956783708362570b17c33676383e7d0aba8f64c
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-12-10 01:34:54 +0900

    가로, 세로모드 변환 실습2 ( Manifest 속성 부여 )

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -1,56 +1,29 @@
 package com.example.parkminhyun.mobileprogramming;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String name;
-    TextView editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showToast("onCreate호출됨.");
-        editText= (TextView) findViewById(R.id.textView);
-        Button button= (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            // 버튼을클릭했을때사용자가입력한값을name 변수에할당
-                name = editText.getText().toString();
-                Toast.makeText(getApplicationContext(), "입력된값을변수에저장했습니다: " + name, Toast.LENGTH_LONG).show();
-            }
-        });
+    }
 
-        // 저장되었던값복원
-        if (savedInstanceState!= null) {// 이화면이초기화될때name 변수의값복원
-            name = savedInstanceState.getString("name");
-            Toast.makeText(getApplicationContext(), "값을복원했습니다: " + name, Toast.LENGTH_LONG).show();
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+// 가로방향전환시처리
+        if (newConfig.orientation== Configuration.ORIENTATION_LANDSCAPE) {
+            showToast("방향: ORIENTATION_LANDSCAPE");
+// 세로방향전환시처리
+        } else if (newConfig.orientation== Configuration.ORIENTATION_PORTRAIT) {
+            showToast("방향: ORIENTATION_PORTRAIT");
         }
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        outState.putString("name", name);// name 변수의값저장
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        showToast("onStart호출됨.");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        showToast("onStop호출됨.");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        showToast("onDestroy호출됨.");
     }
     public void showToast(String data) {
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
