39206b14e2cced55d531233fe1edd5b028d5f861
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-12-10 01:26:53 +0900

    가로, 세로모드 변환 실습

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -2,107 +2,57 @@ package com.example.parkminhyun.mobileprogramming;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
-    GestureDetector detector;// 제스처디텍터객체선언
-    boolean backFlag = false;
-
+    String name;
+    TextView editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView= (TextView) findViewById(R.id.textView);
-
-        // 뷰를 터치했을때 이벤트를 전달 받을 리스너 등록
-        View view= findViewById(R.id.view);
-        View view2 = findViewById(R.id.view2);
-
-        view.setOnTouchListener(new View.OnTouchListener() {
+        showToast("onCreate호출됨.");
+        editText= (TextView) findViewById(R.id.textView);
+        Button button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-
-                float curX= motionEvent.getX();// 터치한곳의좌표
-                float curY= motionEvent.getY();
-
-                if (action == MotionEvent.ACTION_DOWN) {
-                    println("손가락눌림: " + curX+ ", " + curY);
-                }
-                else if (action == MotionEvent.ACTION_MOVE) {
-                    println("손가락움직임: " + curX+ ", " + curY);
-                }
-                else if (action == MotionEvent.ACTION_UP) {
-                    println("손가락뗌: " + curX+ ", " + curY);
-                }return true;}
-        });
-
-        // 뷰를터치했을때발생하는터치이벤트를제스처디텍터로전달
-        view2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                detector.onTouchEvent(motionEvent);
-                return true;
+            public void onClick(View view) {
+            // 버튼을클릭했을때사용자가입력한값을name 변수에할당
+                name = editText.getText().toString();
+                Toast.makeText(getApplicationContext(), "입력된값을변수에저장했습니다: " + name, Toast.LENGTH_LONG).show();
             }
         });
 
-        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent motionEvent) {
-                println("onDown() 호출됨.");
-                return true;
-            }
-            @Override
-            public void onShowPress(MotionEvent motionEvent) {
-                println("onShowPress() 호출됨.");
-            }
-            @Override
-            public boolean onSingleTapUp(MotionEvent motionEvent) {
-                println("onSingleTapUp() 호출됨.");
-                return true;
-            }
-            @Override
-            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                println("onScroll() 호출됨: " + v + ", " + v1);
-                return true;
-            }
-            @Override
-            public void onLongPress(MotionEvent motionEvent) {
-                println("onLongPress() 호출됨.");
-            }
-            @Override
-            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                println("onFling() 호출됨: " + v + ", " + v1);
-                return true;
-            }
-        });
-    }
-    public void println(String data) {
-        textView.append(data + "\n");
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        // 시스템BACK 버튼이눌렸을경우토스트메시지보여주기
-        if(keyCode== KeyEvent.KEYCODE_BACK) {
-
-            if(backFlag)
-                this.finish();
-
-            backFlag = true;
-
-            Toast.makeText(this, "시스템BACK 버튼이눌렸습니다.", Toast.LENGTH_LONG).show();
-            return true;
+        // 저장되었던값복원
+        if (savedInstanceState!= null) {// 이화면이초기화될때name 변수의값복원
+            name = savedInstanceState.getString("name");
+            Toast.makeText(getApplicationContext(), "값을복원했습니다: " + name, Toast.LENGTH_LONG).show();
         }
-        return false;
     }
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("name", name);// name 변수의값저장
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart호출됨.");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop호출됨.");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy호출됨.");
+    }
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
 }
