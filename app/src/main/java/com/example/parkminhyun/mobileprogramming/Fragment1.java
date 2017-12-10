13940467fd12f7c31d9b459be26579ee800b339c
박민현 <algusl92@naver.com>
2017-12-11 03:59:10 +0900

    Fragment + Service 실습

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/Fragment1.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/Fragment1.java
@@ -1,21 +1,36 @@
 package com.example.parkminhyun.mobileprogramming;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by ParkMinHyun on 2017-12-11.
  */
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements View.OnClickListener {
+
+    Button f1button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView= (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+        f1button = (Button) rootView.findViewById(R.id.f1button);
+        f1button.setOnClickListener(this);
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(getActivity().getApplicationContext(), MyService.class);
+        intent.putExtra("command", "show");
+        intent.putExtra("name", "ParkMinHyun");
+        getActivity().startService(intent);
     }
 }
--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.parkminhyun.mobileprogramming;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -54,4 +57,30 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) { }
         });
     }
+
+    public void startBtnClick(View view) {
+        Intent intent= new Intent(this, MyService.class);
+        intent.putExtra("command", "show");
+        intent.putExtra("name", "ParkMinHyun");
+        startService(intent);
+    }
+
+    public void stopBtnClick(View view) {
+    }
+
+    // 액티비티가 이미 메모리에 만들어져있는 경우 onNewIntent() 메소드호출됨
+    @Override
+    protected void onNewIntent(Intent intent) {
+        processIntent(intent);
+        super.onNewIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+        if (intent != null) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+            Toast.makeText(this, "command : " + command + ", name : " + name, Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
