0288fd8ebca1a74572756688e3547ccfc19f01c8
박민현 <algusl92@naver.com>
2017-12-11 02:42:44 +0900

    ListFragment, ViewerFragment 실습

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.parkminhyun.mobileprogramming;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.ImageSelectionCallback {
+    ListFragment listFragment;
+    ViewerFragment viewerFragment;
+    int[] images = {R.drawable.sample0, R.drawable.sample1};
 
-    MainFragment mainFragment;
-    MenuFragment menuFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        mainFragment= (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
-        menuFragment= new MenuFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        listFragment = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        viewerFragment = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment);
     }
 
-    public void onFragmentChanged(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, menuFragment).commit();
-        } else if (index == 1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
-        }
+    @Override
+    public void onImageSelected(int position) {
+        viewerFragment.setImage(images[position]);
     }
 }
