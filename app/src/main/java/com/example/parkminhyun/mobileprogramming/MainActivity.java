71e399fd169544aa470663c2600777cefd818e93
박민현 <algusl92@naver.com>
2017-12-12 03:11:50 +0900

    ActionBar를 통한 Fragment Tab 기능 구현

--- app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/mobileprogramming/MainActivity.java
@@ -7,11 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
+
+    List<Fragment> fragments = new ArrayList<>();
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar= getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
 
         fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        fragments.add(fragment1);
+        fragments.add(fragment2);
+        fragments.add(fragment3);
 
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("통화기록"));
@@ -48,11 +61,43 @@ public class MainActivity extends AppCompatActivity {
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
             }
+
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) { }
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
             @Override
-            public void onTabReselected(TabLayout.Tab tab) { }
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curID = item.getItemId();
+        Fragment selected = null;
+        switch (curID) {
+            case R.id.menu_refresh:
+                if (index != 0) {
+                    selected = fragments.get(--index);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+                }
+                break;
+            case R.id.menu_search:
+                if (index != 2) {
+                    selected = fragments.get(++index);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+                }
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
